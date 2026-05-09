3730729dd97d7a9105f533f865349566deecc487
Lukas <lukas.melgar@tecsup.edu.pe>
2026-05-08 20:43:24 -0500

    Listo para GitHub

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const http = require('http');
+
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type':'text/html'});
-    res.end('<h1>Servidor funcionando correctamente</h1>');
+    res.end('<h1>Servidor funcionando con dotenv</h1>');
 });
 
-server.listen(3000, () => {
-    console.log('Servidor en puerto 3000');
+server.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en puerto ${PORT}`);
 });
